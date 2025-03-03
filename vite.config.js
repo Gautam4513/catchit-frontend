dde435985dfa6a8675ba7f926cb9bd327059851f
Gautam Mandaviya <gautammandaviya21@gmail.com>
2025-03-03 22:14:36 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),tailwindcss()],
   server: {
-    allowedHosts: "all",
+    allowedHosts: "catchit-keval-1.onrender.com",
   },
 })
